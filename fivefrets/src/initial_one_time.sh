5fb0546a893fd4770ae74ce08606c4c762a1a5be
FFPY <vinodronoldbe@gmail.com>
2017-01-01 18:22:23 -0500

    20170101.2

--- fivefrets/src/initial_one_time.sh
+++ fivefrets/src/initial_one_time.sh
@@ -6,6 +6,8 @@ python3 manage.py createsuperuser
 
 python3 manage.py loaddata chords/data.json
 
+python3 manage.py loaddata fivefrets/flatpages.json
+
 locale-gen en_US en_US.UTF-8
 
 echo 'select 149 for UTF-8 and select 3 for en.UTF-8'
--- fivefrets/src/start_app.sh
+++ fivefrets/src/start_app.sh
@@ -3,4 +3,6 @@
 sleep 10
 su -m ffrets -c "python3 manage.py makemigrations"
 su -m ffrets -c "python3 manage.py migrate"
-su -m ffrets -c "python3 manage.py runserver 0.0.0.0:8000"
+su -m ffrets -c "python3 manage.py collectstatic --noinput"
+# su -m ffrets -c "python3 manage.py runserver 0.0.0.0:8000"
+su -m ffrets -c "gunicorn fivefrets.wsgi:application -w 2 -b :8000"
